0340e0a3df7f3d5fdfa6e9fe320cfd47bd7e88d8
Yasin Chowdhury <62109033+emon253@users.noreply.github.com>
2024-05-29 16:02:26 +0600

    initializer added

--- src/main/java/org/carbarn/scrapify/ScrapifyApplication.java
+++ src/main/java/org/carbarn/scrapify/ScrapifyApplication.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScrapifyApplication implements CommandLineRunner {
+public class ScrapifyApplication  extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
@@ -16,23 +17,23 @@ public class ScrapifyApplication implements CommandLineRunner {
     }
 
 
-    @Override
-    public void run(String... args) throws InterruptedException {
-//		scraperClient.runScraper();
-//        scraperClient.startScraperForDearWiseData();
-        ensureScrapperStatusFlag();
-    }
+//    @Override
+//    public void run(String... args) throws InterruptedException {
+////		scraperClient.runScraper();
+////        scraperClient.startScraperForDearWiseData();
+//        ensureScrapperStatusFlag();
+//    }
 
-    @Autowired
-    private FlagRepository flagRepository;
-    public void ensureScrapperStatusFlag() {
-        // Attempt to retrieve the flag, and create/save it as "STOPPED" if not present
-        Flag flag = flagRepository.findByName("AUTOTRADER_SCRAPPER_STATUS")
-                .orElseGet(() -> {
-                    Flag newFlag = new Flag();
-                    newFlag.setName("AUTOTRADER_SCRAPPER_STATUS");
-                    newFlag.setValue("STOPPED");
-                    return flagRepository.save(newFlag);
-                });
-    }
+//    @Autowired
+//    private FlagRepository flagRepository;
+//    public void ensureScrapperStatusFlag() {
+//        // Attempt to retrieve the flag, and create/save it as "STOPPED" if not present
+//        Flag flag = flagRepository.findByName("AUTOTRADER_SCRAPPER_STATUS")
+//                .orElseGet(() -> {
+//                    Flag newFlag = new Flag();
+//                    newFlag.setName("AUTOTRADER_SCRAPPER_STATUS");
+//                    newFlag.setValue("STOPPED");
+//                    return flagRepository.save(newFlag);
+//                });
+//    }
 }
