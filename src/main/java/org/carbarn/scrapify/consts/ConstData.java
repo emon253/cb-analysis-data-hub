8468a089c9835219c09cf52d0644c931548b106c
Yasin Chowdhury <62109033+emon253@users.noreply.github.com>
2024-06-01 09:10:00 +0600

    updated on dealers

--- src/main/java/org/carbarn/scrapify/consts/ConstData.java
+++ src/main/java/org/carbarn/scrapify/consts/ConstData.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class ConstData {
     public static List<Long> getDealers() {
-//        Long[] dealerIdsArray = { 12751L, 936L, 9861L, 5231L, 12918L, 11141L, 12011L, 1007851L, 12510L, 9676L, 1017971L,
-//                1018913L, 1018719L, 11803L, 7909L, 7981L, 4866L, 12834L, 1018495L, 10861L, 13455L, 1019225L, 1018169L, 5548L,1019261L};
-        Long[] dealerIdsArray = {12751L, 936L,1019261L};
+        Long[] dealerIdsArray = { 12751L, 936L, 9861L, 5231L, 12918L, 11141L, 12011L, 1007851L, 12510L, 9676L, 1017971L,
+                1018913L, 1018719L, 11803L, 7909L, 7981L, 4866L, 12834L, 1018495L, 10861L, 13455L, 1019225L, 1018169L, 5548L,1019261L};
+//        Long[] dealerIdsArray = {12751L, 936L,1019261L};
 
         return Arrays.asList(dealerIdsArray);
     }
